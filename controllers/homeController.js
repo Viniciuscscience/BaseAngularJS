0b479f84500d0a2282707aab48dfe14c6fd8ae63
vinicius <vinicius.cscience@gmail.com>
2018-03-05 22:53:08 -0300

    base AngularJS

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,6 +1,7 @@
 app.controller('homeController', function($scope, correios){
 	$scope.homeVariable = "ESTE É A PÁGINA HOME! Digite um CEP e veja o endereço!";
-	$scope.getAddres = function(cep){
+	$scope.getAddress = function(cep){
+		console.log("oi");
 		correios.getLocation(cep).then(function(response){
 			$scope.fullAddress = response.data;
 		});
