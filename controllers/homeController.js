cb5e5cea0d0d4c57aeaa3b90453e86a7990ae0a2
vinicius <vinicius.cscience@gmail.com>
2018-03-05 22:54:54 -0300

    base AngularJS

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,7 +1,6 @@
 app.controller('homeController', function($scope, correios){
 	$scope.homeVariable = "ESTE É A PÁGINA HOME! Digite um CEP e veja o endereço!";
 	$scope.getAddress = function(cep){
-		console.log("oi");
 		correios.getLocation(cep).then(function(response){
 			$scope.fullAddress = response.data;
 		});
--- services/correios.js
+++ services/correios.js
@@ -1,7 +1,7 @@
 app.service('correios', function($http){
 	var obj = {};
 	obj.getLocation = function(cep){
-		return $http.get('http://maps.google.com/maps/api/geocode/json?address=' + cep + '&sensor=false');
+		return $http.get('https://maps.google.com/maps/api/geocode/json?address=' + cep + '&sensor=false');
 	}
 	return obj;
 });
